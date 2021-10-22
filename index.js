7dca275b344d997e0080032e02d7fdc04bca51d8
Fireflydoggo <bull.karli@gmail.com>
2021-10-22 12:23:05 -0700

    added js for variables

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var wins = 0
 var losses = 0
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
-
+var messageEl = document.getElementById('message')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
 // target all .box elements and attach a click event listener to each one using a loop
 
 // within each click event...
